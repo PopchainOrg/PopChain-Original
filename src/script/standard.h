e44c132fe4a72e522f34a16533610a506f9610f2
blackvipernull <40845286+blackvipernull@users.noreply.github.com>
2018-07-12 17:43:05 +0800

    modify this file

--- src/script/standard.h
+++ src/script/standard.h
@@ -24,9 +24,10 @@ public:
     CScriptID(const uint160& in) : uint160(in) {}
 };
 
-static const unsigned int MAX_OP_RETURN_RELAY = 83; //! bytes (+1 for OP_RETURN, +2 for the pushdata opcodes)
 extern bool fAcceptDatacarrier;
 extern unsigned nMaxDatacarrierBytes;
+static const unsigned int MAX_OP_RETURN_RELAY = 83; //! bytes (+1 for OP_RETURN, +2 for the pushdata opcodes)
+
 
 /**
  * Mandatory script verification flags that all new blocks must comply with for
