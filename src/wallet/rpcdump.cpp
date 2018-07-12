1a8bce5deaa6066b23817fcdca7c9515a45b0973
beiolo <40843607+beiolo@users.noreply.github.com>
2018-07-12 17:54:30 +0800

    Update rpcdump.cpp

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -30,19 +30,6 @@ std::string static EncodeDumpTime(int64_t nTime) {
 
 void EnsureWalletIsUnlocked();
 
-int64_t static DecodeDumpTime(const std::string &str) {
-    static const boost::posix_time::ptime epoch = boost::posix_time::from_time_t(0);
-    static const std::locale loc(std::locale::classic(),
-        new boost::posix_time::time_input_facet("%Y-%m-%dT%H:%M:%SZ"));
-    std::istringstream iss(str);
-    iss.imbue(loc);
-    boost::posix_time::ptime ptime(boost::date_time::not_a_date_time);
-    iss >> ptime;
-    if (ptime.is_not_a_date_time())
-        return 0;
-    return (ptime - epoch).total_seconds();
-}
-
 std::string static EncodeDumpString(const std::string &str) {
     std::stringstream ret;
     BOOST_FOREACH(unsigned char c, str) {
@@ -67,6 +54,19 @@ std::string DecodeDumpString(const std::string &str) {
         ret << c;
     }
     return ret.str();
+}
+
+int64_t static DecodeDumpTime(const std::string &str) {
+    static const boost::posix_time::ptime epoch = boost::posix_time::from_time_t(0);
+    static const std::locale loc(std::locale::classic(),
+        new boost::posix_time::time_input_facet("%Y-%m-%dT%H:%M:%SZ"));
+    std::istringstream iss(str);
+    iss.imbue(loc);
+    boost::posix_time::ptime ptime(boost::date_time::not_a_date_time);
+    iss >> ptime;
+    if (ptime.is_not_a_date_time())
+        return 0;
+    return (ptime - epoch).total_seconds();
 }
 
 UniValue importprivkey(const UniValue& params, bool fHelp)
