38608b903ce1489338f0241cc080baafc456586a
AsherHuu <1195846323@qq.com>
2018-07-16 15:20:02 +0800

    update source files

--- src/claimtrie.h
+++ src/claimtrie.h
@@ -251,19 +251,19 @@ typedef std::pair<std::string, CSupportValue> supportQueueEntryType;
 
 typedef std::map<std::string, supportMapEntryType> supportMapType;
 
-typedef std::vector<outPointHeightType> queueNameRowType;
 typedef std::map<std::string, queueNameRowType> queueNameType;
+typedef std::vector<outPointHeightType> queueNameRowType;
 
 typedef std::vector<nameOutPointHeightType> insertUndoType;
 
-typedef std::vector<nameOutPointType> expirationQueueRowType;
 typedef std::map<int, expirationQueueRowType> expirationQueueType;
+typedef std::vector<nameOutPointType> expirationQueueRowType;
 
-typedef std::vector<claimQueueEntryType> claimQueueRowType;
 typedef std::map<int, claimQueueRowType> claimQueueType;
+typedef std::vector<claimQueueEntryType> claimQueueRowType;
 
-typedef std::vector<supportQueueEntryType> supportQueueRowType;
 typedef std::map<int, supportQueueRowType> supportQueueType;
+typedef std::vector<supportQueueEntryType> supportQueueRowType;
 
 typedef std::map<std::string, CClaimTrieNode*, nodenamecompare> nodeCacheType;
 
