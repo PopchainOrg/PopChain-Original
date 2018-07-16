ed76fc528e8a51b378f8ec82519de7e41ecf2a92
blackvipernull <blackviper@null.net>
2018-07-16 11:23:38 +0800

    update source files

--- src/chain.h
+++ src/chain.h
@@ -3,11 +3,11 @@
 #ifndef BITCOIN_CHAIN_H
 #define BITCOIN_CHAIN_H
 
+#include "pow.h"
+#include "uint256.h"
 #include "arith_uint256.h"
 #include "primitives/block.h"
-#include "pow.h"
 #include "tinyformat.h"
-#include "uint256.h"
 
 #include <vector>
 
