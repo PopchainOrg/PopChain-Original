55124c7916bd904f549ad6e8a7772a257f54f51c
blackvipernull <blackviper@null.net>
2018-07-16 11:15:36 +0800

    update source files

--- src/cachemultimap.h
+++ src/cachemultimap.h
@@ -143,23 +143,7 @@ public:
         map_cit it = mapIndex.find(key);
         return (it != mapIndex.end());
     }
-
-    void Erase(const K& key)
-    {
-        map_it mit = mapIndex.find(key);
-        if(mit == mapIndex.end()) {
-            return;
-        }
-        it_map_t& mapIt = mit->second;
-
-        for(it_map_it it = mapIt.begin(); it != mapIt.end(); ++it) {
-            listItems.erase(it->second);
-            --nCurrentSize;
-        }
-
-        mapIndex.erase(mit);
-    }
-
+	
     void Erase(const K& key, const V& value)
     {
         map_it mit = mapIndex.find(key);
@@ -180,6 +164,22 @@ public:
         if(mapIt.size() < 1) {
             mapIndex.erase(mit);
         }
+    }
+
+    void Erase(const K& key)
+    {
+        map_it mit = mapIndex.find(key);
+        if(mit == mapIndex.end()) {
+            return;
+        }
+        it_map_t& mapIt = mit->second;
+
+        for(it_map_it it = mapIt.begin(); it != mapIt.end(); ++it) {
+            listItems.erase(it->second);
+            --nCurrentSize;
+        }
+
+        mapIndex.erase(mit);
     }
 
     const list_t& GetItemList() const {
