f03aa01f1edf7dea6271c236783f3c2328fe171c
qitown <lialvin@126.com>
2018-07-21 08:52:00 +0800

    update source files

--- src/init.h
+++ src/init.h
@@ -19,13 +19,13 @@ void StartShutdown();
 bool ShutdownRequested();
 /** Interrupt threads */
 void Interrupt(boost::thread_group& threadGroup);
-void Shutdown();
 //!Initialize the logging infrastructure
 void InitLogging();
 //!Parameter interaction: change current parameters depending on various rules
 void InitParameterInteraction();
 bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler);
 void PrepareShutdown();
+void Shutdown();
 
 /** The help message mode determines what help message to show */
 enum HelpMessageMode {
