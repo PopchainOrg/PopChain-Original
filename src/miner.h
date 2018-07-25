d6669de2a75a55bd14a1ad7755881ca1db4df78e
mrdingpop <dingchao_138@163.com>
2018-07-25 15:58:21 +0800

    update source files

--- src/miner.h
+++ src/miner.h
@@ -26,8 +26,6 @@ struct CBlockTemplate
     std::vector<int64_t> vTxSigOps;
 };
 
-/** Run the miner threads */
-void GenerateBitcoins(bool fGenerate, int nThreads, const CChainParams& chainparams);
 /** Generate a new block, without valid proof-of-work */
 CBlockTemplate* CreateNewBlock(const CChainParams& chainparams, const CScript& scriptPubKeyIn);
 /** Modify the extranonce in a block */
