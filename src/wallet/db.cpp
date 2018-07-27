5796587133214df2ccfaea02225f95308e5853bc
qitown <lialvin@126.com>
2018-07-27 11:25:06 +0800

    update source files

--- src/wallet/db.cpp
+++ src/wallet/db.cpp
@@ -77,7 +77,6 @@ bool CDBEnv::Open(const boost::filesystem::path& pathIn)
 
     strPath = pathIn.string();
     boost::filesystem::path pathLogDir = pathIn / "database";
-    TryCreateDirectory(pathLogDir);
     boost::filesystem::path pathErrorFile = pathIn / "db.log";
     LogPrintf("CDBEnv::Open: LogDir=%s ErrorFile=%s\n", pathLogDir.string(), pathErrorFile.string());
 
